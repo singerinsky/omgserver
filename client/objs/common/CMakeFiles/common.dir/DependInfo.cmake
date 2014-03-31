780fa6c69c09c905f715a72e18ed2283c3424677
guanlei <singerinsky@gmail.com>
2014-03-31 10:37:30 +0800

    aaaaaaaaaaaaaaaaaaaaaaaaaa

--- client/objs/common/CMakeFiles/common.dir/DependInfo.cmake
+++ client/objs/common/CMakeFiles/common.dir/DependInfo.cmake
@@ -4,24 +4,25 @@ SET(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 SET(CMAKE_DEPENDS_CHECK_CXX
-  "/home/gl/omgserver/common/CEventThread.cpp" "/home/gl/omgserver/client/objs/common/CMakeFiles/common.dir/CEventThread.cpp.o"
-  "/home/gl/omgserver/common/CPoolThread.cpp" "/home/gl/omgserver/client/objs/common/CMakeFiles/common.dir/CPoolThread.cpp.o"
-  "/home/gl/omgserver/common/CThread.cpp" "/home/gl/omgserver/client/objs/common/CMakeFiles/common.dir/CThread.cpp.o"
-  "/home/gl/omgserver/common/CThreadManage.cpp" "/home/gl/omgserver/client/objs/common/CMakeFiles/common.dir/CThreadManage.cpp.o"
-  "/home/gl/omgserver/common/CTimerEventHandler.cpp" "/home/gl/omgserver/client/objs/common/CMakeFiles/common.dir/CTimerEventHandler.cpp.o"
-  "/home/gl/omgserver/common/IJob.cpp" "/home/gl/omgserver/client/objs/common/CMakeFiles/common.dir/IJob.cpp.o"
-  "/home/gl/omgserver/common/JobDequeue.cpp" "/home/gl/omgserver/client/objs/common/CMakeFiles/common.dir/JobDequeue.cpp.o"
-  "/home/gl/omgserver/common/MD5.cpp" "/home/gl/omgserver/client/objs/common/CMakeFiles/common.dir/MD5.cpp.o"
-  "/home/gl/omgserver/common/circular_buffer.cpp" "/home/gl/omgserver/client/objs/common/CMakeFiles/common.dir/circular_buffer.cpp.o"
-  "/home/gl/omgserver/common/mtrand.cpp" "/home/gl/omgserver/client/objs/common/CMakeFiles/common.dir/mtrand.cpp.o"
-  "/home/gl/omgserver/common/server_application.cpp" "/home/gl/omgserver/client/objs/common/CMakeFiles/common.dir/server_application.cpp.o"
-  "/home/gl/omgserver/common/system_util.cpp" "/home/gl/omgserver/client/objs/common/CMakeFiles/common.dir/system_util.cpp.o"
-  "/home/gl/omgserver/common/timer_manager.cpp" "/home/gl/omgserver/client/objs/common/CMakeFiles/common.dir/timer_manager.cpp.o"
+  "/home/gl/workspace/omgserver/common/CEventThread.cpp" "/home/gl/workspace/omgserver/client/objs/common/CMakeFiles/common.dir/CEventThread.cpp.o"
+  "/home/gl/workspace/omgserver/common/CPoolThread.cpp" "/home/gl/workspace/omgserver/client/objs/common/CMakeFiles/common.dir/CPoolThread.cpp.o"
+  "/home/gl/workspace/omgserver/common/CThread.cpp" "/home/gl/workspace/omgserver/client/objs/common/CMakeFiles/common.dir/CThread.cpp.o"
+  "/home/gl/workspace/omgserver/common/CThreadManage.cpp" "/home/gl/workspace/omgserver/client/objs/common/CMakeFiles/common.dir/CThreadManage.cpp.o"
+  "/home/gl/workspace/omgserver/common/CTimerEventHandler.cpp" "/home/gl/workspace/omgserver/client/objs/common/CMakeFiles/common.dir/CTimerEventHandler.cpp.o"
+  "/home/gl/workspace/omgserver/common/IJob.cpp" "/home/gl/workspace/omgserver/client/objs/common/CMakeFiles/common.dir/IJob.cpp.o"
+  "/home/gl/workspace/omgserver/common/JobDequeue.cpp" "/home/gl/workspace/omgserver/client/objs/common/CMakeFiles/common.dir/JobDequeue.cpp.o"
+  "/home/gl/workspace/omgserver/common/MD5.cpp" "/home/gl/workspace/omgserver/client/objs/common/CMakeFiles/common.dir/MD5.cpp.o"
+  "/home/gl/workspace/omgserver/common/circular_buffer.cpp" "/home/gl/workspace/omgserver/client/objs/common/CMakeFiles/common.dir/circular_buffer.cpp.o"
+  "/home/gl/workspace/omgserver/common/mtrand.cpp" "/home/gl/workspace/omgserver/client/objs/common/CMakeFiles/common.dir/mtrand.cpp.o"
+  "/home/gl/workspace/omgserver/common/server_application.cpp" "/home/gl/workspace/omgserver/client/objs/common/CMakeFiles/common.dir/server_application.cpp.o"
+  "/home/gl/workspace/omgserver/common/system_util.cpp" "/home/gl/workspace/omgserver/client/objs/common/CMakeFiles/common.dir/system_util.cpp.o"
+  "/home/gl/workspace/omgserver/common/timer_manager.cpp" "/home/gl/workspace/omgserver/client/objs/common/CMakeFiles/common.dir/timer_manager.cpp.o"
   )
 SET(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 SET(CMAKE_TARGET_DEFINITIONS
+  "_MY_DEBUG_"
   "_MY_DEBUG_"
   )
 
@@ -32,8 +33,8 @@ SET(CMAKE_TARGET_LINKED_INFO_FILES
 # The include file search paths:
 SET(CMAKE_C_TARGET_INCLUDE_PATH
   "../include"
-  "/home/gl/omgserver/common/../include"
-  "/home/gl/omgserver/common/.."
+  "/home/gl/workspace/omgserver/common/../include"
+  "/home/gl/workspace/omgserver/common/.."
   )
 SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
 SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- db_server/dbmsg_process.cpp
+++
@@ -1,7 +0,0 @@
-#include "dbmsg_process.h"
-
-void dbmsg_process::msg_process(MsgBase* msg_base,socket_client* client)
-{
-
-
-}
